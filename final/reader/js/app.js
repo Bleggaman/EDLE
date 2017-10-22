948e446d10959b8e2cdc6840cea4c3384f936d81
Zuko09 <varunknath@gmail.com>
2017-10-22 14:06:08 -0700

    Buttons work

--- final/reader/js/app.js
+++ final/reader/js/app.js
@@ -8,7 +8,7 @@ app.controller('myCtrl', function($scope) {
         // entry: data object
         return Object.keys(query).every((key) => entry[key].toLowerCase().includes(query[key].toLowerCase()));
       }
-      
+
       $scope.entryToElement = function entryToElement(entry) {
         var ul = $("<ul></ul>");
         for (var key in entry) {
@@ -19,26 +19,50 @@ app.controller('myCtrl', function($scope) {
         ans.append(ul);
         return ans;
       }
-      
+
       var data = [
         {"name": "jake", "jobTitle":"doctor", "company":"goodhospital", "state":"washington", "city": "seattle"},
         {"name": "jim", "jobTitle":"webdev", "company":"google", "state":"san francisco", "city": "california"}
       ];
-    
+
+      data = [
+    {
+      "bio" : "",
+      "company" : "Your Company",
+      "degree" : "",
+      "jobTitle" : "Your Profession",
+      "location" : "Your City, State",
+      "name" : "Enter Name Here",
+      "profile_pic" : ""
+    },
+    {
+      "bio" : "",
+      "company" : "Edle",
+      "degree" : "",
+      "facts" : [ "I have been a Fizz jungle one trick pony since season 2", "I have beaten a 2200 elo chess player" ],
+      "jobTitle" : "Co-founder",
+      "location" : "Seattle, WA",
+      "name" : "Varun Viswanath",
+      "profile_pic" : ""
+    }];
+
       $scope.name = "";
-      $scope.jobTitle =""; 
+      $scope.jobTitle ="";
       $scope.company = "";
-      $scope.state = ""; 
+      $scope.state = "";
       $scope.city = "";
 
       $scope.results = []
 
       $scope.submit = function submit() {
+          var database = firebase.database();
+          console.log(database.ref("users").once('value'));
+
           var query = {"name" :$scope.name, "jobTitle": $scope.jobTitle, "company": $scope.company, "state": $scope.state, "city": $scope.city};
           $scope.results = [];
           $scope.results = data.filter((entry) => $scope.isMatch(query, entry));
         }
-        
+
         $("a[data-toggle=\"tab\"]").click(function(e) {
           e.preventDefault();
           $(this).tab("show");
